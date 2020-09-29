1a2c92ea48f0c14dea707f21d9119ad22af680d9
Israel Carlos <israelcarlos01@gmail.com>
2020-09-29 16:42:15 -0300

    bot de música

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require the discord.js module
 const Discord = require("discord.js");
-const { token, prefix } = require("./config.json");
+const { token, prefix, link } = require("./config.json");
+const ytdl = require("ytdl-core");
+
 // create a new Discord client
 const client = new Discord.Client();
 
@@ -8,6 +10,33 @@ const client = new Discord.Client();
 // this event will only trigger one time after logging in
 client.once("ready", () => {
   console.log("Ready!");
+});
+
+//Mensagem de boas vindas para um membro novo.
+client.on("guildMemberAdd", (member) => {
+  member.send(`Agora você é um TCHOLA!`);
+});
+
+// Bot de música
+client.on("message", async (message) => {
+  if (message.content === "!play") {
+    // Variável que contém o canal de voz que o usuário está atualmente
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.reply("Entre em um canal de voz primeiro!");
+    }
+
+    const connection = await voiceChannel.join();
+    const watcher = connection.play(
+      ytdl("https://www.youtube.com/watch?v=tNcMrdAeJeU&ab_channel=LilWhind", {
+        filter: "audioonly",
+        quality: "highest",
+      })
+    );
+
+    watcher.on("end", () => voiceChannel.leave());
+  }
 });
 
 client.on("message", (message) => {
@@ -18,6 +47,10 @@ client.on("message", (message) => {
     message.channel.send(
       `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`
     );
+  }
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel) {
+    return message.reply("Entre em um canal de voz primeiro!");
   }
 });
 
