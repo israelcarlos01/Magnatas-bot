b40cc8bc44ece688419a205666a1327e2e001874
Israel Carlos <israelcarlos01@gmail.com>
2020-10-07 14:14:18 -0300

    mute and desmute

--- index.js
+++ index.js
@@ -17,6 +17,51 @@ client.on("guildMemberAdd", (member) => {
   member.send(`Agora você é um TCHOLA!`);
 });
 
+//Funcionalidade para mutar geral
+client.on("message", async (message) => {
+  const voiceChannel = message.member.voice.channel;
+  const hasPermission = message.member.hasPermission([
+    "MANAGE_ROLES",
+    "ADMINISTRATOR",
+  ]);
+
+  if (hasPermission) {
+    if (message.content === "!mute") {
+      for (let member of voiceChannel.members) {
+        member[1].voice.setMute(true);
+      }
+    } else if (message.content === "!unmute") {
+      for (let member of voiceChannel.members) {
+        member[1].voice.setMute(false);
+      }
+    }
+  } else {
+    message.reply("você não tem permissão para executar o comando!!");
+  }
+});
+
+client.on("message", async (message) => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  //if (!message.guild) return;
+  const voiceChannel = message.member.voice.channel;
+
+  if (voiceChannel) {
+    if (message.content === "!carine") {
+      const connection = await message.member.voice.channel.join();
+      connection.play("audio/carine.mp3");
+    } else if (message.content === "!calate") {
+      const connection = await message.member.voice.channel.join();
+      connection.play("audio/calate.mp3");
+    } else if (message.content === "!brbr") {
+      const connection = await message.member.voice.channel.join();
+      connection.play("audio/brbr.mp3");
+    }
+  } else {
+    message.reply("You need to join a voice channel first!");
+  }
+});
+
 // Bot de música
 client.on("message", async (message) => {
   const musica = message.content;
@@ -39,6 +84,12 @@ client.on("message", async (message) => {
       })
     );
 
+    /*if (message.content === "!stop") {
+      watcher.on.
+    } else if (message.content === "!resume") {
+      watcher.resume();
+    }*/
+
     watcher.on("end", () => voiceChannel.leave());
   }
 });
@@ -52,10 +103,10 @@ client.on("message", (message) => {
       `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`
     );
   }
-  const voiceChannel = message.member.voice.channel;
+  /*const voiceChannel = message.member.voice.channel;
   if (!voiceChannel) {
     return message.reply("Entre em um canal de voz primeiro!");
-  }
+  }*/
 });
 
 // login to Discord with your app's token
